df5729912eb50c1625767c8502c1ea230ffaac10
Austin Sloane <sloane.ats@gmail.com>
2017-09-05 22:48:57 -0700

    finished implementation of adding friends and rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,100 +1,112 @@
 // YOUR CODE HERE:
 var App = function() {
   this.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
-  this.username = window.location.search.slice(10);
-  this.lastId = null;
+  this.username = 'anonymous';
   this.roomname = 'lobby';
+  this.messages = [];
+  this.lastMessageId = null;
+  this.friends = {};
 };
 
 
 App.prototype.init = function( ) {
-  var context = this;
+  // Get username
+  this.username = window.location.search.substr(10);
 
-  $( document ).ready(function() {
+  // Cache jQuery selectors
+  this.$message = $('#messageInput');
+  this.$chats = $('#chats');
+  this.$roomSelect = $('#roomSelect');
+  this.$send = $('#send');
 
-    $('#messageInput').keypress(function(key) {
-      if ( key.which === 13 ) {
-        var text = $('#messageInput').val();
-        var username = context.username;
-        app.send({username, text});
-        $('#messageInput').val('');
-      }
-    });
+  // Add event listeners
+  this.$send.on('submit', this.handleSubmit);
+  this.$roomSelect.on('change', this.handleRoomChange);
+  this.$chats.on('click', '.username', this.handleUsernameClick);
 
-    $('button').on('click', function() {
-      var text = $('#messageInput').val();
-      var username = context.username;
-      app.send({username, text});
-      $('#messageInput').val('');
-    });
-    
-    $('#chats').on('click', '.username', function() {
-      var name = $(this).data('user');
-      console.log('clicked', name);
-      app.handleUsernameClick(name);
-    });
-
-  });
-
+  // Fetch previous messages
   this.fetch();
 
+  // Poll for new messages
   setInterval(function() {
+    this.clearMessages();
     this.fetch();
-  }.bind(this), 800);
+  }.bind(this), 3000);
 };
 
 App.prototype.send = function(message) {
+  this.startSpinner();
+  var context = this;
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: this.server,
     type: 'POST',
     // processData: false,
     data: JSON.stringify(message),
-    // data: message,
     contentType: 'application/JSON',
     success: function (data) {
-      console.log('chatterbox: Message sent');
+      context.$message.val('');
+      context.fetch();
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to send message', data);
     }
   });
-};    
+};
 
 App.prototype.fetch = function(link) {
+  this.startSpinner();  
   var context = this;
   $.ajax({
-    // This is the url you should use to communicate with the parse API server.
     url: this.server,
     type: 'GET',
     data: {order: '-createdAt'},
     success: function (data) {
-      data.results.forEach( function(message) {
-        if ( context.lastId !== message.objectId ) {
-          context.renderMessage(message);
-          context.lastId = message.objectId;
-        }
-      });
+      if ( !data.results || !data.results.length ) { return; }
+      context.messages = data.results;
+      let mostRecentMessage = app.messages[app.messages.length - 1];
+      if ( mostRecentMessage.objectId !== context.lastMessageId ) {
+        context.renderMessages(context.messages);
+        context.renderRoomList(context.messages);
+      }
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to send message', data);
     }
   });
-}; 
+};
 
-App.prototype.clearMessages = function() {
-  $('#chats').html('');
+App.prototype.renderMessages = function(messages) {
+  this.clearMessages();
+  messages
+    .filter(function(message) {
+      if ( app.roomname === 'lobby' && !message.roomname ) {
+        return true;
+      } else if ( message.roomname === app.roomname ) {
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .forEach(this.renderMessage);
+
+  this.stopSpinner();
 };
 
 App.prototype.renderMessage = function({username, text, roomname}) {
+  var room = roomname ? roomname : 'lobby';
   var $message = $('<div></div>');
   $message.addClass('chat');
   var $username = $('<div></div>');
-  $username.addClass('username');
-  $username.attr('data-user', username);
-  $username.text(username);
+  $username
+    .addClass('username')
+    .attr('data-user', username)
+    .text(username);
+  if ( app.friends[username] === true ) {
+    $username.toggleClass('friend');
+  }
   var $text = $('<p></p>');
   $text.text(text);
   $message.append($username);
@@ -102,38 +114,75 @@ App.prototype.renderMessage = function({username, text, roomname}) {
   $('#chats').append($message);
 };
 
-App.prototype.renderRoom = function(room) {
-  var $room = $('<div></div>');
-  $room.attr('id', room);
-  $('#roomSelect').append($room);
+App.prototype.clearMessages = function() {
+  $('#chats').html('');
 };
 
-App.prototype.handleUsernameClick = function(username) {
-  this.friendList[username] = username;
+App.prototype.renderRoomList = function(messages) {
+  this.$roomSelect.html('<option value="__newRoom">New Room...</option>');
+
+  if ( messages ) {
+    var rooms = {};
+    messages.forEach( message => {
+      var roomname = message.roomname;
+      if ( roomname && !rooms[roomname]) {
+        rooms[roomname] = true;
+        app.renderRoom(roomname);
+      }
+    });
+  }
+
+  this.$roomSelect.val(app.roomname);
 };
 
-App.prototype.handleSubmit = function() {
+App.prototype.renderRoom = function(roomname) {
+  var $option = $('<option/>').val(roomname).text(roomname);
+  app.$roomSelect.append($option);
+};
 
+App.prototype.handleRoomChange = function(event) {
+  var selectedIndex = app.$roomSelect.prop('selectedIndex');
+  if ( selectedIndex === 0 ) {
+    var roomname = prompt('Enter a new room name');
+    if ( roomname ) {
+      app.roomname = roomname;
+      app.renderRoom(roomname);
+      app.$roomSelect.val(roomname);
+    }
+  } else {
+    app.roomname = app.$roomSelect.val();
+  }
+  app.renderMessages(app.messages);
+};
+
+App.prototype.handleSubmit = function(event) {
+  var username = app.username;
+  var text = app.$message.val();
+  var roomname = app.roomname || 'lobby';
+  app.send({username, text, roomname});
+  event.preventDefault();
+};
+
+App.prototype.handleUsernameClick = function(event) {
+  var username = $(event.target).data('user');
+
+  if ( username !== undefined ) {
+    app.friends[username] = !app.friends[username];
+    var $usernames = $(`[data-user="${username}"]`).toggleClass('friend');
+  }
+};
+
+App.prototype.startSpinner = function() {
+  $('.spinner').show();
+  $('.form input[type=submit]').attr('disabled', true);
+};
+
+App.prototype.stopSpinner = function() {
+  $('.spinner').fadeOut('fast');
+  $('.form input[type=submit]').attr('disabled', null);
 
 };
 
 var app = new App();
 
 app.init();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
